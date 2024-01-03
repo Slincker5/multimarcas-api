fb65f617924e1eb6ee6e6e4dfa76c1489af09475
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-03 00:30:57 -0600

    eliminar publicaciones

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -71,16 +71,16 @@ class Post extends Database
 
     public function listPost()
     {
-        #$sql = 'SELECT * FROM publicaciones ORDER BY fecha DESC';
-        $sql = 'SELECT p.*, COUNT(l.id) AS num_likes
+        $sql = 'SELECT p.*, COUNT(l.id) AS num_likes, COUNT(c.id) AS num_comments
             FROM publicaciones p
             LEFT JOIN likes l ON p.post_uuid = l.post_uuid
+            LEFT JOIN comentarios c ON p.post_uuid = c.post_uuid
             GROUP BY p.post_uuid
             ORDER BY p.fecha DESC';
+
         $list = $this->ejecutarConsulta($sql);
         $posts = $list->fetchAll(\PDO::FETCH_ASSOC);
         return $posts;
-
     }
 
     public function selectPost($post_uuid)
