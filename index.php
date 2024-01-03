9657dbebb4fe2cf6054f9590f7d7b1434610bf78
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-02 23:06:10 -0600

    like en publicaciones

--- index.php
+++ index.php
@@ -149,7 +149,7 @@ $app->group('/post', function ($group) {
     $group->delete('/remove', PostController::class . ':removePost');
     $group->post('/like', PostController::class . ':likePost');
     $group->get('/list-like', PostController::class . ':listLikePost');
-    $group->post('/select-post/{post_uuid}', PostController::class . ':selectPost');
+    $group->get('/select-post/{post_uuid}', PostController::class . ':selectPost');
 
 })->add($validateJwtMiddleware);
 
