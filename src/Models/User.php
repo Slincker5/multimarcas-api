954aabdb8fcca1dad9cdb00cf72d6bf89b2244db
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-20 00:48:16 -0600

    uodate tokens

--- src/Models/User.php
+++ src/Models/User.php
@@ -103,8 +103,8 @@ class User extends Database
         $token = JWT::encode($payload, $this->key, $alg);
         $this->response['status'] = 'OK';
         $this->response['message'] = 'token generado con exito.';
-        $this->response["usera"] = $datos;
-        $this->response['token'] = $token[0]["suscripcion"];
+        $this->response["usera"] = $datos[0]["suscripcion"];
+        $this->response['token'] = $token;
         return $this->response;
     }
 
