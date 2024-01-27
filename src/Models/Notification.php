f5b10fe6af86b4bac2164361a94d931c5520e120
Gerson Borja <gerson.borja2018@gmail.com>
2024-01-27 17:45:54 -0600

    notificaciones

--- src/Models/Notification.php
+++ src/Models/Notification.php
@@ -4,34 +4,36 @@ namespace App\Models;
 
 use Pusher\PushNotifications\PushNotifications;
 
-class Notification {
+class Notification
+{
 
-    public function crearNotificacion($titulo, $cuerpo, $imagen='https://cintillos-plazamundo.netlify.app/android-chrome-192x192.png') {
+    public function crearNotificacion($titulo, $cuerpo, $imagen = 'https://cintillos-plazamundo.netlify.app/android-chrome-192x192.png')
+    {
 
         $beamsClient = new PushNotifications(array(
-            "instanceId" => "90b80143-5f43-4ed9-a447-8ad08e3ca889",
-            "secretKey" => "B76E6AA02AA068FB1525A42D8A7734DE2CF669DEBCCB4421DB380844762DD706",
+            "instanceId" => "b963f891-0b89-4e01-84a8-698b97373219",
+            "secretKey" => "B99ABC7E749BD28709D779B4EEF1D9F3E0A1A303BC57E7A58F4F2DCCB0FE8B28",
         ));
 
         $data = array(
             "title" => $titulo,
             "body" => $cuerpo,
             "icon" => $imagen,
-            "deep_link" => "https://cintillos-plazamundo.netlify.app"
+            "deep_link" => "https://cintillos-plazamundo.netlify.app",
         );
 
-        $publishResponse = $beamsClient->publishToInterests(
-            array("cintillos"),
+        $beamsClient->publishToInterests(
+            array("2c62e966-63d8-4bfd-832e-89094ae47eec"),
             array(
                 "web" => array(
-                    "notification" => $data
+                    "notification" => $data,
                 ),
             )
         );
 
         $mensaje = [
             "status" => "ok",
-            "message" => "Notificacion enviada"
+            "message" => "Notificacion enviada",
         ];
 
         return $mensaje;
