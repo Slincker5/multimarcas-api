c83b1596711152110d6eb0971e58a4df52247b0d
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-11 22:39:59 -0600

    se agrego la validacion de la suscripcion

--- src/Models/Poster.php
+++ src/Models/Poster.php
@@ -59,7 +59,8 @@ class Poster extends Database
 
         date_default_timezone_set("America/El_Salvador");
         $estadoPremium = $this->instanciaPremium->validarSuscripcion($this->user_uuid);
-        if ($suscripcion === 0  && $estadoPremium) {
+        $suscripcion = $suscripcion ? $suscripcion : $estadoPremium;
+        if ($suscripcion) {
             $this->response['status'] = 'error';
             $this->response['message'] = 'Necesitas ser usuario premiun para esta accion';
             return $this->response;
