e7616ba528796f595f8a6cc2a04ce3e17e5b8c4a
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-19 00:37:31 -0600

    se mejoro la validaciones de premiun;

--- notis.php
+++ notis.php
@@ -13,7 +13,7 @@ foreach($lista as $user) {
         $fechaInicio = new DateTime(); // Fecha actual
         $fechaFin = new DateTime($user['fin_suscripcion']); // Fecha de fin de suscripción del usuario
         $diferencia = $fechaInicio->diff($fechaFin); // Diferencia entre las fechas
-        echo "Días restantes: " . $diferencia->days . " días<br>"; // Muestra la diferencia en días
+        echo "Días restantes: " . $diferencia . " días<br>"; // Muestra la diferencia en días
     } catch (Exception $e) {
         echo "Excepción capturada: ",  $e->getMessage(), "<br>";
     }
