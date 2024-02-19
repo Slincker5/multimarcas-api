89f2851e324498e58906af0985cc0066c587ce87
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-19 01:08:48 -0600

    se mejoro la validaciones de premiun;

--- notis.php
+++ notis.php
@@ -1,35 +1,26 @@
 <?php
 
-use App\Models\Notification;
 use App\Models\User;
+use App\Models\Notification;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $users = new User();
 
 $lista = $users->notificarPremium();
-
-$usersNotificados = []; // Arreglo para mantener un registro de usuarios notificados
-
-foreach ($lista as $user) {
+$listaDos = [];
+foreach($lista as $user) {
     try {
         $fechaInicio = new DateTime(); // Fecha actual
         $fechaFin = new DateTime($user['fin_suscripcion']); // Fecha de fin de suscripción del usuario
         $diferencia = $fechaInicio->diff($fechaFin); // Diferencia entre las fechas
         if ($diferencia->days == 2 && $fechaFin > $fechaInicio) {
-            if (!in_array($user['user_uuid'], $usersNotificados)) { // Verificar si el usuario ya fue notificado
-                echo "hola" . $user["user_uuid"] . "<br>";
-                #$instanciaNotificacion = new Notification();
-                #$cuerpoNotificacion = "Prepárate para renovar y seguir disfrutando de nuestros servicios sin interrupciones.";
-                #$instanciaNotificacion->crearNotificacion("🔔👀!SOLO TIENES 3 DIAS!", $cuerpoNotificacion); // Crear notificación
-                #echo "Faltan " . $diferencia->days . " días para que la suscripción de " . $user['user_uuid'] . " termine.<br>"; // Muestra la diferencia en días
-#
-                array_push($usersNotificados, $user['user_uuid']); // Agregar el usuario al arreglo de notificados
-            }
+            $instanciaNotificacion = new Notification();
+            $cuerpoNotificacion = "Prepárate para renovar y seguir disfrutando de nuestros servicios sin interrupciones.";
+            $instanciaNotificacion->crearNotificacionUsers($user["user_uuid"], "🔔👀!SOLO TIENES 3 DIAS!", $cuerpoNotificacion); // Verifica si faltan exactamente 3 días
+            echo "Faltan " . $diferencia->days . " días para que la suscripción de " . $user['user_uuid'] . " termine.<br>"; // Muestra la diferencia en días
         }
     } catch (Exception $e) {
-        echo "Excepción capturada: ", $e->getMessage(), "<br>";
+        echo "Excepción capturada: ",  $e->getMessage(), "<br>";
     }
 }
-
-echo json_encode($usersNotificados);
--- src/Models/Notification.php
+++ src/Models/Notification.php
@@ -40,4 +40,36 @@ class Notification
 
     }
 
+    public function crearNotificacionUsers($interes, $titulo, $cuerpo, $imagen = 'https://cintillos-plazamundo.netlify.app/android-chrome-192x192.png')
+        {
+    
+            $beamsClient = new PushNotifications(array(
+                "instanceId" => "b963f891-0b89-4e01-84a8-698b97373219",
+                "secretKey" => "B99ABC7E749BD28709D779B4EEF1D9F3E0A1A303BC57E7A58F4F2DCCB0FE8B28",
+            ));
+    
+            $data = array(
+                "title" => $titulo,
+                "body" => $cuerpo,
+                "icon" => $imagen,
+                "deep_link" => "https://cintillos-plazamundo.netlify.app",
+            );
+    
+            $beamsClient->publishToInterests(
+                array($interes),
+                array(
+                    "web" => array(
+                        "notification" => $data,
+                    ),
+                )
+            );
+    
+            $mensaje = [
+                "status" => "ok",
+                "message" => "Notificacion enviada",
+            ];
+    
+            return $mensaje;
+    
+        }
 }
