39cafc019a51a6d8e1f87f9a2eccd8b3e8c81a9e
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-19 00:43:06 -0600

    se mejoro la validaciones de premiun;

--- notis.php
+++ notis.php
@@ -9,14 +9,12 @@ $lista = $users->notificarPremium();
 $listaDos = [];
 foreach($lista as $user) {
     try {
-        echo "Fin de suscripción: " . $user['fin_suscripcion'] . "<br>"; // Depuración
         $fechaInicio = new DateTime(); // Fecha actual
         $fechaFin = new DateTime($user['fin_suscripcion']); // Fecha de fin de suscripción del usuario
         $diferencia = $fechaInicio->diff($fechaFin); // Diferencia entre las fechas
-        if($diferencia->days == 3){
-            echo "Días restantes: " . $diferencia->days . " días<br>"; // Muestra la diferencia en días
+        if ($diferencia->days == 3 && $fechaFin > $fechaInicio) { // Verifica si faltan exactamente 3 días
+            echo "Faltan " . $diferencia->days . " días para que la suscripción de " . $user['nombre'] . " termine.<br>"; // Muestra la diferencia en días
         }
-        
     } catch (Exception $e) {
         echo "Excepción capturada: ",  $e->getMessage(), "<br>";
     }
