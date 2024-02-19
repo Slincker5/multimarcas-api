c462addf4cc9d848c152d642de6c168c99d013c6
Gerson Borja <gerson.borja2018@gmail.com>
2024-02-19 00:21:23 -0600

    se mejoro la validaciones de premiun;

--- notis.php
+++ notis.php
@@ -2,6 +2,17 @@
 
 use App\Models\User;
 require __DIR__ . '/vendor/autoload.php';
+
 $users = new User();
 
-echo json_encode($users->notificarPremium());
+$lista = $users->notificarPremium();
+$listaDos = [];
+foreach($lista as $user) { // Corrected here
+    $fechaInicio = new DateTime(); // Today's date
+    $fechaFin = new DateTime($user['fin_suscripcion']); // Subscription end date from $user array
+    $diferencia = $fechaInicio->diff($fechaFin); 
+    if($diferencia === 3){
+        array_push($listaDos, $user);
+    }
+}
+print($listaDos);
