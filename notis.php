378d4212e72a2c681cd9a1f6b10869c6bbd74e8f
Gerson Borja <gerson.borja2018@gmail.com>
2024-03-03 00:44:05 -0600

    ok

--- notis.php
+++ notis.php
@@ -6,11 +6,14 @@ use App\Models\Notification;
 require __DIR__ . '/vendor/autoload.php';
 
 $users = new User();
-
+$noti = new Notification();
+$titulo = "🏷️AFICHES CON DESCUENTO";
+$cuerpo = "Ya estan disponible la creacion de afiches con porcentajes.";
 $lista = $users->notificarPremium();
 foreach($lista as $user) {
     try {
-        var_dump($user["username"]);
+        $noti->crearNotificacionUsers($user["user_uuid"], $titulo, $cuerpo);
+        echo "enviada a " . $user["username"] . "<br>";
     } catch (Exception $e) {
         echo "Excepción capturada: ",  $e->getMessage(), "<br>";
     }
