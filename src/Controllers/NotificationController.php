a19dabdec7db25dd7bc2c5e5d8c9a5f47cf4fd4c
Gerson Borja <gerson.borja2018@gmail.com>
2024-04-23 22:17:27 -0600

    se agreggo las notificaciones firebase y se elimino pusher beams rev 5

--- src/Controllers/NotificationController.php
+++ src/Controllers/NotificationController.php
@@ -37,6 +37,9 @@ class NotificationController
 
     function sendGlobal($request, $response, $args)
     {
+        $user_rol = $request->getAttribute('payload')->data->rol;
+
+        if($user_rol == 'Admin'){
             $body = $request->getParsedBody();
             $title = $body["title"];
             $message = $body["body"];
@@ -45,5 +48,12 @@ class NotificationController
             $response->withHeader('Content-Type', 'application/json');
             $response->getBody()->write(json_encode($content));
             return $response;
+        }else{
+            $this->res['status'] = 'error';
+            $this->res['message'] = 'No tienes el rango para el envio de notificaciones';
+            $response->withHeader('Content-Type', 'application/json');
+            $response->getBody()->write(json_encode($this->res));
+            return $response;
+        }
     }
 }
