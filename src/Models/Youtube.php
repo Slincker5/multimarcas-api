e22d2250aa9a7208e8adb315dea72ba922b8e66e
Gerson Borja <gerson.borja2018@gmail.com>
2024-05-05 04:37:10 -0600

    add new class and crontroller for downloader yt

--- src/Models/Youtube.php
+++ src/Models/Youtube.php
@@ -29,12 +29,11 @@ class Youtube
             return;
         }
     
-        // Especificar correctamente el template de salida para 'yt-dlp'
         $ytDlpCommand = escapeshellcmd("/var/multimarcas-dev/bin/yt-dlp --format bestaudio[ext=webm] https://www.youtube.com/watch?v=$videoId -o '".escapeshellarg("%(id)s.%(ext)s")."'");
         exec($ytDlpCommand, $outputYTDL, $returnYTDL);
     
         if ($returnYTDL === 0) {
-            $webmFile = "$videoId.webm";  // Construye el nombre del archivo asumiendo que el comando fue exitoso
+            $webmFile = "$videoId.webm";  // Asume que se ha descargado correctamente
     
             if (file_exists($webmFile)) {
                 $mp3File = preg_replace('/\.webm$/', '.mp3', $webmFile);
@@ -42,12 +41,15 @@ class Youtube
                 exec($ffmpegConvertCommand, $outputConvert, $returnConvert);
     
                 if ($returnConvert === 0 && file_exists($mp3File)) {
+                    // Limpiar cualquier buffer de salida previo
+                    ob_end_clean();
+    
                     // Configurar cabeceras para descarga de archivo
                     header('Content-Type: audio/mpeg');
                     header('Content-Disposition: attachment; filename="' . basename($mp3File) . '"');
                     header('Content-Length: ' . filesize($mp3File));
     
-                    // Leer y enviar el archivo de forma eficiente
+                    // Abrir el archivo en modo binario
                     $fp = fopen($mp3File, 'rb');
                     fpassthru($fp);
                     fclose($fp);
@@ -55,6 +57,8 @@ class Youtube
                     // Borrar los archivos después de enviarlos
                     unlink($mp3File);
                     unlink($webmFile);
+    
+                    exit;
                 } else {
                     echo "Error en la conversión a MP3: " . implode("\n", $outputConvert);
                 }
