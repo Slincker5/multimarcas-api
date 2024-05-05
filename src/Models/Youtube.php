0d90c94af32628198760864c9d3270f86accff31
Gerson Borja <gerson.borja2018@gmail.com>
2024-05-04 23:05:51 -0600

    add new class and crontroller for downloader yt

--- src/Models/Youtube.php
+++ src/Models/Youtube.php
@@ -4,7 +4,7 @@ namespace App\Models;
 class Youtube {
 
     public function searchYouTube($searchQuery) {
-        $command = escapeshellcmd("python3 buscar-yt.py '" . addslashes($searchQuery) . "'");
+        $command = escapeshellcmd("/home/admin/multimarcas-api-dev/python3 buscar-yt.py '" . addslashes($searchQuery) . "'");
         $output = shell_exec($command);
         $arr = json_decode($output, true);
         return $arr;
