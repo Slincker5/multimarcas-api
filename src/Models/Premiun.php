c6facb7fe373f8a8d258b3cc6a0eaa3599f5c253
Slincker5 <hipersocial08@gmail.com>
2024-06-23 20:04:56 -0600

    test gift user top

--- src/Models/Premiun.php
+++ src/Models/Premiun.php
@@ -237,7 +237,7 @@ class Premiun extends Database
     public function awardTopWeek()
     {
         foreach ($this->instanceUser->getTopAll() as $user) {
-            echo $user . "<br>";
+            echo var_dump($user) . "<br>";
         }
     }
 }
